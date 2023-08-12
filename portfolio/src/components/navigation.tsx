6be036503036da87d86c1ac83896f643269e7fb2
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-12 19:14:30 +0100

    footer added

--- portfolio/src/components/navigation.tsx
+++ portfolio/src/components/navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     <nav className="bg-gray-900 p-4">
       <ul className="flex space-x-4">
         <li><a href="#home" className="text-white">Home</a></li>
-        <li><a href="#bio" className="text-white">Bio</a></li> {/* Updated ID */}
+        <li><a href="#bio" className="text-white">About Me</a></li> {/* Updated ID */}
         <li><a href="#projects" className="text-white">Projects</a></li> {/* Added Projects link */}
         <li><a href="#contact" className="text-white">Contact Me</a></li> {/* Added Contact Me link */}
       </ul>
