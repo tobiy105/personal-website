85ffe37b9457560e9456a53ce72ba27e411cdff8
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-15 07:18:17 +0100

    fixed picture appear above the navbar while scroll

--- portfolio/src/components/sildedownnav.tsx
+++ portfolio/src/components/sildedownnav.tsx
@@ -1,14 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../logo.png';
+
 const SlideDownNav = () => {
-  const [scrolling, setScrolling] = useState(false);
+  const [activeTab, setActiveTab] = useState('home');
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 20) {
-        setScrolling(true);
-      } else {
-        setScrolling(false);
+      const homeSection = document.getElementById('home');
+      const aboutmeSection = document.getElementById('aboutme');
+      const projectsSection = document.getElementById('projects');
+      const contactSection = document.getElementById('contact');
+
+      if (
+        homeSection && aboutmeSection && projectsSection && contactSection &&
+        window.scrollY >= homeSection.offsetTop &&
+        window.scrollY < aboutmeSection.offsetTop
+      ) {
+        setActiveTab('home');
+      } else if (
+        homeSection && aboutmeSection && projectsSection &&
+        window.scrollY >= aboutmeSection.offsetTop &&
+        window.scrollY < projectsSection.offsetTop
+      ) {
+        setActiveTab('aboutme');
+      } else if (
+        aboutmeSection && projectsSection && contactSection &&
+        window.scrollY >= projectsSection.offsetTop &&
+        window.scrollY < contactSection.offsetTop
+      ) {
+        setActiveTab('projects');
+      } else if (
+        contactSection &&
+        window.scrollY >= contactSection.offsetTop
+      ) {
+        setActiveTab('contact');
       }
     };
 
@@ -19,16 +44,32 @@ const SlideDownNav = () => {
   }, []);
 
   return (
-    <nav className="bg-[rgb(4,28,76)] p-4 fixed top-0 w-full flex items-center justify-between">
+    <nav className="bg-gray-900 p-4 fixed top-0 w-full flex items-center justify-between z-10"> {/* Add z-10 to ensure the navbar stays on top */}
       <div className="flex items-center">
         <img src={logo} className="w-8 h-8 mr-2" alt="Logo" />
         <h1 className="text-white text-lg font-semibold">Oluwatobi Sobola</h1>
       </div>
       <ul className="flex space-x-4">
-        <li><a href="#home" className="text-white">Home</a></li>
-        <li><a href="#bio" className="text-white">About Me</a></li>
-        <li><a href="#projects" className="text-white">Projects</a></li>
-        <li><a href="#contact" className="text-white">Contact Me</a></li>
+        <li>
+          <a href="#" className={`text-white ${activeTab === 'home' ? 'font-semibold' : ''}`}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#aboutme" className={`text-white ${activeTab === 'aboutme' ? 'font-semibold' : ''}`}>
+            About Me
+          </a>
+        </li>
+        <li>
+          <a href="#projects" className={`text-white ${activeTab === 'projects' ? 'font-semibold' : ''}`}>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className={`text-white ${activeTab === 'contact' ? 'font-semibold' : ''}`}>
+            Contact Me
+          </a>
+        </li>
       </ul>
     </nav>
   );
