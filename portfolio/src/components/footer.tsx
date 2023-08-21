3c71241a8cde0a28cb82989279b63b8a509793a9
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-21 20:43:09 +0100

    css change

--- portfolio/src/components/footer.tsx
+++ portfolio/src/components/footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='mt-16 py-6 text-center bg-accent'>
-        <p className='text-white'>© Oluwatobi Sobola | All rights reserved 2023 </p>
+    <div className='mt-16 py-6 text-center bg-gray-700 bg-opacity-80'>
+        <p className='text-white'> Oluwatobi Sobola | All rights reserved 2023 </p>
     </div>
   )
 }
