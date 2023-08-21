62f7dd3aa7232543fd1dd18d4637aba5012b87ed
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-21 21:57:03 +0100

    narvar for smaller screens

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -7,7 +7,8 @@ export const Navbar = () => {
 
   const [activeTab, setActiveTab] = useState('home');
   const [navbarTransparent, setNavbarTransparent] = useState(true);
-
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  
   useEffect(() => {
     const handleScroll = () => {
       const homeSection = document.getElementById('home');
@@ -58,41 +59,60 @@ export const Navbar = () => {
     };
   }, []);
 
-  return (
-    
-      <div className={`pt-8 px-4 py-4  fixed flex justify-between top-0 z-10 w-full ${navbarTransparent ?  'bg-transparent' : 'bg-gray-700 bg-opacity-20'}`}>
-          <div className='text-4xl font-medium'>Oluwatobi</div>
-          
-          <ul className='gap-10 lg:gap-16 hidden md:flex '>
-              <li >
-                <a href="#home" className={` text-2xl text-white ${activeTab === 'home' ? 'font-bold' : ''}`}>
-                  Home
-                </a>
-              </li>
-              <li >
-                <a href="#about" className={`text-2xl text-white ${activeTab === 'about' ? 'font-bold' : ''}`}>
-                  About Me
-                </a>
-              </li>
-              <li >
-                <a href="#projects" className={`text-2xl text-white ${activeTab === 'projects' ? 'font-bold' : ''}`}>
-                  Projects
-                </a>
-              </li>
-              <li >
-                <a href="#work" className={`text-2xl text-white ${activeTab === 'work' ? 'font-bold' : ''}`}>
-                  Work Experience
-                </a>
-              </li>
-              <li >
-                <a href="#contact" className={`text-2xl text-white ${activeTab === 'contact' ? 'font-bold' : ''}`}>
-                  Contact
-                </a>
-              </li>
-          </ul>
+  const handleMobileMenuClick = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
 
-          <AiOutlineMenu className='md:hidden'  size={30}/>    
-      </div>
-    
-  )
+  return (
+    <div className={`pt-8 px-4 py-4 fixed flex justify-between top-0 z-10 w-full ${navbarTransparent ? 'bg-transparent' : 'bg-gray-700 bg-opacity-20'}`}>
+      <div className='text-xl md:text-4xl font-medium'>Oluwatobi</div>
+      <ul className={`gap-10 lg:gap-16 hidden md:flex ${showMobileMenu ? 'hidden' : ''}`}>
+        <li >
+          <a href="#home" className={` text-2xl text-white ${activeTab === 'home' ? 'font-bold' : ''}`}>
+            Home
+          </a>
+        </li>
+        <li >
+          <a href="#about" className={`text-2xl text-white ${activeTab === 'about' ? 'font-bold' : ''}`}>
+            About Me
+          </a>
+        </li>
+        <li >
+          <a href="#projects" className={`text-2xl text-white ${activeTab === 'projects' ? 'font-bold' : ''}`}>
+            Projects
+          </a>
+        </li>
+        <li >
+          <a href="#work" className={`text-2xl text-white ${activeTab === 'work' ? 'font-bold' : ''}`}>
+            Work Experience
+          </a>
+        </li>
+        <li >
+          <a href="#contact" className={`text-2xl text-white ${activeTab === 'contact' ? 'font-bold' : ''}`}>
+            Contact
+          </a>
+        </li>
+      </ul>
+      <AiOutlineMenu className='md:hidden' size={20} onClick={handleMobileMenuClick} />
+      {showMobileMenu && (
+        <div className='md:hidden mt-2'>
+          <a href="#home" className={`block text-white text-xl ${activeTab === 'home' ? 'font-bold' : ''}`}>
+            Home
+          </a>
+          <a href="#about" className={`block text-white text-xl ${activeTab === 'about' ? 'font-bold' : ''}`}>
+            About Me
+          </a>
+          <a href="#projects" className={`block text-white text-xl ${activeTab === 'projects' ? 'font-bold' : ''}`}>
+            Projects
+          </a>
+          <a href="#work" className={`block text-white text-xl ${activeTab === 'work' ? 'font-bold' : ''}`}>
+            Work Experience
+          </a>
+          <a href="#contact" className={`block text-white text-xl ${activeTab === 'contact' ? 'font-bold' : ''}`}>
+            Contact
+          </a>
+        </div>
+      )}
+    </div>
+  );
 };
