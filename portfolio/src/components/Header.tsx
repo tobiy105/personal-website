85f3a36724bbef2da9f9c56e1981532e0625309e
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-21 21:34:34 +0100

    css changes for smaller screens

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -38,42 +38,38 @@ export const Header = () => {
   }, [typingText, displayedText, typingIndex]);
 
   return (
-    <div className=' min-h-screen bg-cover'>
-      
+    <div className='min-h-screen bg-cover'>
       <Navbar />
       <div className='container grid lg:grid-cols-1 h-[calc(100vh-60px)]' id='home'>
-        <div className='text-[80px] sm:text-[130px] font-bold leading-tight lg:gap-16 hidden md:flex justify-center items-center flex-col'>
-          <h1 className="text-white text-5xl font-sagona mb-4">
+        <div className='flex flex-col justify-center items-center h-full'>
+          <h1 className="text-white text-3xl md:text-5xl font-sagona mb-4">
             Hello! 
-            <span className="text-5xl waving-hand">
+            <span className="text-3xl md:text-5xl waving-hand">
               👋
             </span> 
             My name is
             <br />
-            <span className="text-5xl font-bold font-sagona ">Oluwatobi Sobola</span>
+            <span className="text-3xl md:text-5xl font-bold font-sagona ">Oluwatobi Sobola</span>
           </h1>
 
           <div className="writer h-[5rem]">
-            <div className="text-white text-5xl font-sagona writer-text">
+            <div className="text-white text-3xl md:text-5xl font-sagona writer-text">
               {displayedText}
             </div>
           </div>
           <div className="flex space-x-4 mt-2 h-[5rem]">
             <a href="mailto:tobiy105@outlook.com" className="text-gray-100 hover:text-gray-900">
-              <FaEnvelope className="text-4xl" />
+              <FaEnvelope className="text-3xl md:text-4xl" />
             </a>
             <a href="https://www.linkedin.com/in/oluwatobi-sobola" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-900">
-              <FaLinkedin className="text-gray-100 text-4xl hover:text-gray-900" />
+              <FaLinkedin className="text-gray-100 text-3xl md:text-4xl hover:text-gray-900" />
             </a>
             <a href="https://github.com/tobiy105" target="_blank" rel="noopener noreferrer" className="text-gray-100 hover:text-gray-900">
-              <FaGithub className="text-4xl" />
+              <FaGithub className="text-3xl md:text-4xl" />
             </a>
           </div>
         </div>
       </div>
-      {/* Smaller screens*/}
-      
-      
     </div>
   );
 };
