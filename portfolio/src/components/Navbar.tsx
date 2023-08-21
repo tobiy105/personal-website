0ffe16f3cb2414f9f7e8861721cd109fb0f7555a
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-22 00:42:26 +0100

    changing tabs names

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
       const homeSection = document.getElementById('home');
       const aboutmeSection = document.getElementById('about');
       const projectsSection = document.getElementById('projects');
-      const workSection = document.getElementById('work');
+      const skillsSection = document.getElementById('skills');
       const contactSection = document.getElementById('contact');
 
       if (
@@ -36,14 +36,14 @@ export const Navbar = () => {
       ) {
         setActiveTab('projects');
       } else if (
-        projectsSection && contactSection && workSection &&
-        window.scrollY >= workSection.offsetTop &&
+        projectsSection && contactSection && skillsSection &&
+        window.scrollY >= skillsSection.offsetTop &&
         window.scrollY < projectsSection.offsetTop
       ) {
-        setActiveTab('work');
+        setActiveTab('skills');
       } else if (
-        workSection &&
-        window.scrollY >= workSection.offsetTop
+        skillsSection &&
+        window.scrollY >= skillsSection.offsetTop
       ) {
         setActiveTab('contact');
       }
@@ -65,7 +65,7 @@ export const Navbar = () => {
 
   return (
     <div className={`pt-8 px-4 py-4 fixed flex justify-between top-0 z-10 w-full ${navbarTransparent ? 'bg-transparent' : 'bg-gray-700 bg-opacity-20'}`}>
-      <div className='text-xl md:text-4xl font-medium'>Oluwatobi</div>
+      <div className='text-2xl md:text-4xl font-medium'>Oluwatobi Sobola</div>
       <ul className={`gap-10 lg:gap-16 hidden md:flex ${showMobileMenu ? 'hidden' : ''}`}>
         <li >
           <a href="#home" className={` text-2xl text-white ${activeTab === 'home' ? 'font-bold' : ''}`}>
@@ -83,8 +83,8 @@ export const Navbar = () => {
           </a>
         </li>
         <li >
-          <a href="#work" className={`text-2xl text-white ${activeTab === 'work' ? 'font-bold' : ''}`}>
-            Work Experience
+          <a href="#skills" className={`text-2xl text-white ${activeTab === 'skills' ? 'font-bold' : ''}`}>
+            Skills 
           </a>
         </li>
         <li >
@@ -105,8 +105,8 @@ export const Navbar = () => {
           <a href="#projects" className={`block text-white text-xl ${activeTab === 'projects' ? 'font-bold' : ''}`}>
             Projects
           </a>
-          <a href="#work" className={`block text-white text-xl ${activeTab === 'work' ? 'font-bold' : ''}`}>
-            Work Experience
+          <a href="#skills" className={`block text-white text-xl ${activeTab === 'skills' ? 'font-bold' : ''}`}>
+            skills Experience
           </a>
           <a href="#contact" className={`block text-white text-xl ${activeTab === 'contact' ? 'font-bold' : ''}`}>
             Contact
