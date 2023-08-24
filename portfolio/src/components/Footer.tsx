4f3ded084f6eef8c6786e5f7ae1f9e2d78a2fdaa
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-24 16:05:46 +0100

    install sharp

---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa'; // Import icons from 'react-icons/fa'
+
+export const Footer = () => {
+  return (
+    <div className='mt-16 py-6 text-center bg-gray-700 bg-opacity-80'>
+        <p className='text-white'> © Oluwatobi Sobola | All rights reserved 2023 </p>
+      <div className="mt-4 flex items-center justify-center space-x-4">
+        <a href="mailto:tobiy105@outlook.com" className="text-white">
+          <FaEnvelope className="text-lg" /> 
+        </a>
+        <a href="https://linkedin.com/in/oluwatobi-sobola" target="_blank" rel="noopener noreferrer" className="text-white">
+          <FaLinkedin className="text-lg" />
+        </a>
+        <a href="https://github.com/tobiy105" target="_blank" rel="noopener noreferrer" className="text-white">
+          <FaGithub className="text-lg" />
+        </a>
+      </div>
+    </div>
+    
+    
+  )
+}
---
+++ portfolio/src/components/GooeyBackground.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import Goo from 'gooey-react';
+
+const getRandomValue = (min: number, max: number) => Math.random() * (max - min) + min;
+const getRandomColor = () => {
+    const colors = ['rgba(192, 233, 239)', 'rgba(208, 164, 255)','rgba(200, 199, 247)', 'rgba(196, 216, 243)', 'rgba(204, 181, 251)'];
+    return colors[Math.floor(Math.random() * colors.length)];
+};
+
+export const GooeyBackground: React.FC = () => {
+  const [svgAnimationsInitialized, setSvgAnimationsInitialized] = useState(false);
+
+  useEffect(() => {
+      // Initialize your SVG animations or other side effects here
+      if (!svgAnimationsInitialized) {
+          // Your SVG animations setup code goes here
+          // For example, you might set up timers, start animations, etc.
+          
+          setSvgAnimationsInitialized(true); // Set the state to indicate that animations are initialized
+      }
+
+      return () => {
+          // Cleanup or teardown logic for your animations can go here
+      };
+  }, [svgAnimationsInitialized]);
+
+    const gradientColors = ['rgba(255, 196, 0, 0.9)', 'rgba(255, 153, 204, 0.9)'];
+    const gradientDirection = 'to bottom right';
+
+    const [numCircles, setNumCircles] = useState(15);
+    const [svgWidth, setSvgWidth] = useState(0);
+    const [svgHeight, setSvgHeight] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSvgWidth(window.innerWidth);
+            setSvgHeight(window.innerHeight);
+
+            const newNumCircles = Math.floor((window.innerWidth / 800) * 15);
+            setNumCircles(newNumCircles);
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const gradient = `linear-gradient(${gradientDirection}, ${gradientColors.join(', ')})`;
+
+    const circles: JSX.Element[] = [];
+
+    for (let i = 0; i < numCircles; i++) {
+      const cx = `${getRandomValue(10, 90)}%`;
+      const cy = `${getRandomValue(10, 90)}%`;
+      const r = getRandomValue(20, 60);
+      const color = getRandomColor();
+      const animationDuration = `${getRandomValue(8, 15)}s`;
+
+      circles.push(
+          <circle
+              key={i}
+              cx={cx}
+              cy={cy}
+              r={r}
+              fill={color}
+              style={{
+                  animation: `move-${i} ${animationDuration} linear infinite`,
+              }}
+          />
+      );
+    }
+
+    return (
+        <div
+            style={{
+                background: gradient,
+                width: '100vw',
+                height: '100vh',
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                zIndex: -1,
+            }}
+        >
+            <Goo intensity="strong">
+                <svg width={svgWidth} height={svgHeight}>
+                    <g>
+                        {circles}
+                        <defs>
+                            <style>
+                                {circles.map(
+                                    (_, i) => `
+                                    @keyframes move-${i} {
+                                        0%, 100% {
+                                            transform: translate(${getRandomValue(-30, 30)}%, ${getRandomValue(-30, 30)}%);
+                                        }
+                                        50% {
+                                            transform: translate(${getRandomValue(-30, 30)}%, ${getRandomValue(-30, 30)}%) scale(1.2);
+                                        }
+                                    }
+                                    `
+                                )}
+                            </style>
+                        </defs>
+                    </g>
+                </svg>
+            </Goo>
+        </div>
+    );
+};
+
