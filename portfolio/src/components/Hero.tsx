fa05c8e3608a92418d3c365edf07eb7d921fee5a
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-25 08:40:16 +0100

    removing typewrite for small screens

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -4,11 +4,10 @@ import React, { useState, useEffect } from 'react';
 import { Navbar } from './Navbar';
 import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa'; 
 
-
 export const Hero = () => {
   const [typingIndex, setTypingIndex] = useState(0);
   const messages = [
-    'I am a Software Engineer',
+    'Aspiring Software Developer',
     'I Build Websites',
     'I Build Other Projects',
     'I Solve Problems'
@@ -16,34 +15,39 @@ export const Hero = () => {
 
   const [typingText, setTypingText] = useState('');
   const [displayedText, setDisplayedText] = useState('');
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (typingText.length === 0) {
-        setTypingText(messages[typingIndex]);
-      } else {
-        setDisplayedText(displayedText + typingText[0]);
-        setTypingText(typingText.substring(1));
-      }
+    setIsSmallScreen(window.innerWidth <= 640);
 
-      if (typingText.length === 0 && typingIndex === messages.length - 1) {
-        setDisplayedText('');
-        setTypingIndex(0);
-      } else if (typingText.length === 0) {
-        setDisplayedText('');
-        setTypingIndex(typingIndex + 1);
+    const interval = setInterval(() => {
+      if (!isSmallScreen) {
+        if (typingText.length === 0) {
+          setTypingText(messages[typingIndex]);
+        } else {
+          setDisplayedText(displayedText + typingText[0]);
+          setTypingText(typingText.substring(1));
+        }
+
+        if (typingText.length === 0 && typingIndex === messages.length - 1) {
+          setDisplayedText('');
+          setTypingIndex(0);
+        } else if (typingText.length === 0) {
+          setDisplayedText('');
+          setTypingIndex(typingIndex + 1);
+        }
       }
     }, 250);
 
     return () => clearInterval(interval);
-  }, [typingText, displayedText, typingIndex, messages]);
+  }, [typingText, displayedText, typingIndex, messages, isSmallScreen]);
 
   return (
     <div className='min-h-screen bg-cover'>
       <Navbar />
       <div className='container grid lg:grid-cols-1 h-[calc(100vh-60px)]' id='home'>
         <div className='flex flex-col justify-center items-center h-full'>
-          <h1 className="text-white text-3xl md:text-5xl font-sagona mb-4">
+        <h1 className="text-white text-3xl md:text-5xl font-sagona mb-4">
             Hello! 
             <span className="text-3xl md:text-5xl waving-hand">
               👋
@@ -53,11 +57,14 @@ export const Hero = () => {
             <span className="text-3xl md:text-5xl font-bold font-sagona ">Oluwatobi Sobola</span>
           </h1>
 
-          <div className="writer h-[5rem]">
-            <div className="text-white text-3xl md:text-5xl font-sagona writer-text">
-              {displayedText}
+          {!isSmallScreen && (
+            <div className="writer h-[5rem]">
+              <div className="text-white text-3xl md:text-5xl font-sagona writer-text">
+                {displayedText}
+              </div>
             </div>
-          </div>
+          )}
+
           <div className="flex space-x-4 mt-2 h-[5rem]">
             <a href="mailto:tobiy105@outlook.com" className="text-gray-100 hover:text-gray-900">
               <FaEnvelope className="text-3xl md:text-4xl" />
@@ -74,3 +81,5 @@ export const Hero = () => {
     </div>
   );
 };
+
+
