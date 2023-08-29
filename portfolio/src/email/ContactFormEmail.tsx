b5e063dcad5f3df6204905585c28ebd88bf303d0
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-29 08:34:50 +0100

    ESLint error fix

--- portfolio/src/email/ContactFormEmail.tsx
+++ portfolio/src/email/ContactFormEmail.tsx
@@ -23,7 +23,7 @@ export default function ContactFormEmail({
                         <Heading className='leading-tight'>You have received the following message from the contact form</Heading>
                         <Text>{message}</Text>
                         <Hr />
-                        <Text>The sender's email is: {senderEmail}</Text>
+                        <Text>{`The sender's email is: ${senderEmail}`}</Text>
                     </Section>
                 </Container>
             </Body>
