5f95b849f314c5049e899aa8b58566f484db2e62
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-08-29 11:39:17 +0100

    error messages

--- portfolio/src/components/Contact.tsx
+++ portfolio/src/components/Contact.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
-import { FiMail } from 'react-icons/fi';
+'use client'
+
+import React, { useState } from 'react';
 import { BsTelephone } from 'react-icons/bs';
 import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa'; 
 import { SendEmail } from '../api/SendEmail';
 
 export const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    msg: '',
+  });
+
+  const isEmailValid = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [isEmailErrorVisible, setIsEmailErrorVisible] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!isEmailValid(formData.email)) {
+      setIsEmailErrorVisible(true);
+      return;
+    }
+
+    setIsEmailErrorVisible(false);
+
+    const formDataObject = new FormData();
+    formDataObject.append('name', formData.name);
+    formDataObject.append('email', formData.email);
+    formDataObject.append('msg', formData.msg);
+
+    await SendEmail(formDataObject);
+
+    setFormData({
+      name: '',
+      email: '',
+      msg: '',
+    });
+    setIsFormSubmitted(false);
+  };
+  
+
   return (
     <div className='pt-32 container' id='contact'>
       <div className='bg-gray-700 bg-opacity-60 border border-accent p-8 rounded-md'>    
@@ -42,51 +99,61 @@ export const Contact = () => {
 
           </div>
 
-          <div className='space-y-8'>
-            <form className='space-y-8' action={async (formData) => {
-                await SendEmail(formData);
-              }}>
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='name'>Name</label>
-                <input 
-                  className='h-[40px] bg-transparent border border-accent rounded-md' 
-                  type='text' 
-                  id='name' 
-                  name='name'
-                  required
-                  maxLength={50}
-                />
-              </div>
+          <form className='space-y-8' onSubmit={handleSubmit}>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='name'>Name</label>
+              <input 
+                className='h-[40px] bg-transparent border border-accent rounded-md px-2 py-1' 
+                type='text' 
+                id='name' 
+                name='name'
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+                maxLength={50}
+              />
+            </div>
 
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='email'>Email</label>
-                <input 
-                  className='h-[40px] bg-transparent border border-accent rounded-md' 
-                  type='text' 
-                  id='email' 
-                  name='email'
-                  maxLength={50}
-                  required
-                />
-              </div>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='email'>Email</label>
+              <input 
+                className='h-[40px] bg-transparent border border-accent rounded-md px-2 py-1' 
+                type='text' 
+                id='email' 
+                name='email'
+                value={formData.email}
+                onChange={handleInputChange}
+                maxLength={50}
+                required
+              />
+            </div>
 
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='msg'>Message</label>
-                <textarea
-                  className='bg-transparent border border-accent rounded-md' 
-                  id='msg' 
-                  name='msg'
-                  rows={8}
-                  required
-                  maxLength={500}
-                ></textarea>
-              </div>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='msg'>Message</label>
+              <textarea
+                className='bg-transparent border border-accent rounded-md px-2 py-1' 
+                id='msg' 
+                name='msg'
+                rows={8}
+                value={formData.msg}
+                onChange={handleTextareaChange}
+                required
+                maxLength={500}
+              ></textarea>
+            </div>
 
-              <button className='bg-gray-700 bg-opacity-40 border border-accent text-white p-2 px-6 rounded-md hover:bg-gray-700' type='submit'>
-                Send
-              </button>
-            </form>
-          </div>
+            <button
+              className='bg-gray-700 bg-opacity-40 border border-accent text-white p-2 px-6 rounded-md hover:bg-gray-700'
+              type='submit'
+            >
+              Send
+            </button>
+            {isEmailErrorVisible && (
+              <p className="text-red-500">Please enter a valid email address.</p>
+            )}
+            {isFormSubmitted && <p>Message sent successfully!</p>}
+          </form>
+          
         </div>
       </div>
     </div>
